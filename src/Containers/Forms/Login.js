62cdc336498e04aba6c68d49a99c6f9e092c14c0
Kuba Sukowski <kubasulek2@gmail.com>
2020-01-22 15:05:06 +0100

    Finish LoginForm

--- src/Containers/Forms/Login.js
+++ src/Containers/Forms/Login.js
@@ -14,7 +14,9 @@ const styles = (({ palette, spacing }) => ({
 		width: '100%'
 	},
 	errorMessage: {
-		margin: 8
+		margin: 8,
+		minHeight: 35,
+		fontSize: 12,
 	},
 	inputContainer: {
 		display: 'flex',
@@ -67,23 +69,30 @@ const initialState = {
 		rules: {
 			regex: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 		},
-		valid: false
+		valid: false,
+		touched: false,
+		hint: 'Provide a valid email address.'
 	},
 	password: {
 		value: '',
 		rules: {
 			regex: /^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!*/\\[\]`~().,:;"']).*$/,
 		},
-		valid: false
+		valid: false,
+		touched: false,
+		hint: 'Password must have 8 to 20 characters, contain at least one lower and uppercase letter, one digit and one special sign.'
 	},
 	confirmPassword: {
 		value: '',
 		rules: {
 			identity: 'password',
 		},
-		valid: false
+		valid: false,
+		touched: false,
+		hint: 'Passwords must match.'
 	},
-	valid: false
+	valid: false,
+	hint: ''
 };
 
 class LoginForm extends Component {
@@ -93,43 +102,65 @@ class LoginForm extends Component {
 	componentDidMount() {
 		this.emailRef.current.focus();
 	}
+
 	componentDidUpdate(prevProps) {
 		if (prevProps.type !== this.props.type) {
-			this.emailRef.current.focus();
 			this.setState(initialState);
 		}
 	}
+
 	handleInputChange(event, type) {
+		let valid = this.props.type === 'signup' ? this.handleInputValidation(event.target.value, type) : true;
 		this.setState({
 			[type]: {
 				...this.state[type],
-				value: event.target.value
-			}
-		});
+				value: event.target.value,
+				valid: valid,
+				touched: false
+			},
+			hint: ''
+		}, this.handleFormValidation);
 	}
-	handleInputValidation(event, type) {
-		let validInput = false;
+
+	handleInputValidation(value, type) {
 		switch (Object.keys(this.state[type].rules)[0]) {
 			case 'regex':
-				validInput = this.state[type].rules.regex.test(event.target.value);
-				break;
-
+				return this.state[type].rules.regex.test(value);
 			default:
-				validInput = event.target.value === this.state.password.value;
-				break;
-		}
+				return this.state.password.value === value;
 
+		}
+	}
+
+	handleFormValidation() {
+		let keys = Object.keys(this.state).slice(0, -2),
+			allValid;
+
+		keys = this.props.type === 'login' ? keys.slice(0, -1) : keys;
+		allValid = keys.every(key => this.state[key].valid === true);
+
+		return this.setState({ valid: allValid });
+	}
+
+	handleInputFocus = (type) => {
+		return this.setState({
+			hint: this.state[type].hint,
+
+		});
+	}
+	handleInputBlur = (type) => {
 		return this.setState({
 			[type]: {
 				...this.state[type],
-				valid: validInput
-			}
-		}, this.handleFormValidation);
+				touched: true
+			},
+
+		});
 	}
-	handleFormValidation() {
-		let keys = Object.keys(this.state).slice(0, -1);
-		keys = this.props.type === 'login' ? keys.slice(0, -1) : keys;
-		return this.setState({valid: keys.every(key => this.state[key].valid === true)});
+
+	handleFormSubmit(event){
+		event.preventDefault();
+		// send data to server
 	}
 
 	render() {
@@ -140,18 +171,21 @@ class LoginForm extends Component {
 			<form className={classes.root} id='login-form'>
 				<Typography
 					className={classes.errorMessage}
-					color='error'
+					color='primary'
 					variant='subtitle2'
 					align='center'
 				>
+					{this.state.hint}
 				</Typography>
 				<div className={classes.inputContainer}>
 					<TextField
 						className={classes.textField}
 						inputRef={this.emailRef}
 						value={this.state.email.value}
+						error={!this.state.email.valid && this.state.email.touched}
 						onChange={(event) => this.handleInputChange(event, 'email')}
-						onBlur={(event) => this.handleInputValidation(event, 'email')}
+						onBlur={this.props.type === 'signup' ? () => this.handleInputBlur('email') : null}
+						onFocus={this.props.type === 'signup' ? () => this.handleInputFocus('email') : null}
 						placeholder="Email"
 						variant="outlined"
 						type='email'
@@ -172,8 +206,10 @@ class LoginForm extends Component {
 					<TextField
 						className={classes.textField}
 						value={this.state.password.value}
+						error={!this.state.password.valid && this.state.password.touched}
 						onChange={(event) => this.handleInputChange(event, 'password')}
-						onBlur={(event) => this.handleInputValidation(event, 'password')}
+						onBlur={this.props.type === 'signup' ? () => this.handleInputBlur('password') : null}
+						onFocus={this.props.type === 'signup' ? () => this.handleInputFocus('password') : null}
 						placeholder="Password"
 						variant="outlined"
 						type='password'
@@ -195,8 +231,10 @@ class LoginForm extends Component {
 						? <TextField
 							className={classes.textField}
 							value={this.state.confirmPassword.value}
+							error={!this.state.confirmPassword.valid && this.state.confirmPassword.touched}
 							onChange={(event) => this.handleInputChange(event, 'confirmPassword')}
-							onBlur={(event) => this.handleInputValidation(event, 'confirmPassword')}
+							onBlur={this.props.type === 'signup' ? () => this.handleInputBlur('confirmPassword') : null}
+							onFocus={this.props.type === 'signup' ? () => this.handleInputFocus('confirmPassword') : null}
 							placeholder="Confirm password"
 							variant="outlined"
 							type='password'
